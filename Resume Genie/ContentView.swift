be52b2e82d4a15ef88323a622aece508f1b7883b
Fenil <fenil.dobariya@outlook.com>
2024-05-24 13:07:55 -0400

    Initial Commit

---
+++ Resume Genie/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Resume Genie
+//
+//  Created by Fenil Patel on 5/24/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Resume Genie/Resume_GenieApp.swift
@@ -0,0 +1,17 @@
+//
+//  Resume_GenieApp.swift
+//  Resume Genie
+//
+//  Created by Fenil Patel on 5/24/24.
+//
+
+import SwiftUI
+
+@main
+struct Resume_GenieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
